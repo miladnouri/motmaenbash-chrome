1e8ca141b9149b804b5eecc639560ec4abdcf4e1
Milad <milad.nu@gmail.com>
2023-12-10 21:58:40 +0330

    Remove cbinasim.ir payment gateway

--- assets/js/content-script.js
+++ assets/js/content-script.js
@@ -1,6 +1,6 @@
 var url = new URL(location.href);
 // alert(url.hostname);
-if( (url.hostname=='ipg.cbinasim.ir' || url.hostname.match(/\.shaparak\.ir$/i) ) && url.protocol == "https:" ){
+if( (url.hostname.match(/\.shaparak\.ir$/i) ) && url.protocol == "https:" ){
   var overlayDiv=document.createElement("div");
   overlayDiv.id = "motmaenBashCornerSign";
   overlayDiv.innerHTML = '<a href="https://motmaenbash.milad.nu" target="_blank"><img title="این درگاه پرداخت معتبر و امن است" id="motmaenBashCornerSignLogo" src="' + chrome.runtime.getURL('assets/images/sign.png') + '"/></a>'
