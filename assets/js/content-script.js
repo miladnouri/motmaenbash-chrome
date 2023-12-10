29584292168a35f092ef2a60a9dfb0d369c9bece
Milad <milad.nu@gmail.com>
2023-12-10 21:25:48 +0330

    init

---
+++ assets/js/content-script.js
@@ -0,0 +1,8 @@
+var url = new URL(location.href);
+// alert(url.hostname);
+if( (url.hostname=='ipg.cbinasim.ir' || url.hostname.match(/\.shaparak\.ir$/i) ) && url.protocol == "https:" ){
+  var overlayDiv=document.createElement("div");
+  overlayDiv.id = "motmaenBashCornerSign";
+  overlayDiv.innerHTML = '<a href="https://motmaenbash.milad.nu" target="_blank"><img title="این درگاه پرداخت معتبر و امن است" id="motmaenBashCornerSignLogo" src="' + chrome.runtime.getURL('assets/images/sign.png') + '"/></a>'
+  document.body.insertBefore(overlayDiv, document.body.firstChild);
+}
